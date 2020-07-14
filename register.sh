b3c540fb26bf7a239b72b307dba7142c18ca6d6d
JfuentesR <infojose20@gmail.com>
2020-07-14 13:46:45 +0200

    typo

--- register.sh
+++ register.sh
@@ -1,10 +1,10 @@
 # First get a TOKEN
 # export TOKEN='<TOKEN>'
 # export AUTH="x-auth-token: $TOKEN"
-http POST http://paas:6000/bigdata/api/v1/products name=postgresql version=9.3 description='Sophisticated open-source Object-Relational DBMS supporting almost all SQL constructs, including subselects, transactions, and user-defined types. BETA' logo_url='http://hadoop.cesga.es/img/postgresql-icon.gif' x-auth-token:$TOKEN
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/9.3/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/9.3/options --data-binary @options.json -H "$AUTH"
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/9.3/orchestrator --data-binary @fabfile.py -H "$AUTH"
+http POST http://paas:6000/bigdata/api/v1/products name=postgresqlbeta version=9.3 description='Sophisticated open-source Object-Relational DBMS supporting almost all SQL constructs, including subselects, transactions, and user-defined types. BETA' logo_url='http://hadoop.cesga.es/img/postgresql-icon.gif' x-auth-token:$TOKEN
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresqlbeta/9.3/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresqlbeta/9.3/options --data-binary @options.json -H "$AUTH"
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresqlbeta/9.3/orchestrator --data-binary @fabfile.py -H "$AUTH"
 
 # Launching
 # http POST http://paas:6000/bigdata/api/v1/products/postgresql/9.3 password="verysecret" x-auth-token:$TOKEN

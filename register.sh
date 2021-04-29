4499138d5a27d2e4f89d5806f8a690e954418bc6
JfuentesR <infojose20@gmail.com>
2021-04-29 12:24:03 +0200

    register_changes

--- register.sh
+++ register.sh
@@ -2,11 +2,11 @@
 # export TOKEN='<TOKEN>'
 # export AUTH="x-auth-token: $TOKEN"
 
-http POST http://paas:6000/bigdata/api/v1/products name=postgresql version=9.2.15 description='Sophisticated open-source Object-Relational DBMS supporting almost all SQL constructs, including subselects, transactions, and user-defined types.' logo_url='http://hadoop.cesga.es/img/postgresql-icon.gif' x-auth-token:$TOKEN
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/9.2.15/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/9.2.15/options --data-binary @options.json -H "$AUTH"
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/9.2.15/orchestrator --data-binary @fabfile.py -H "$AUTH"
+http POST http://paas:6000/bigdata/api/v1/products name=postgresql version=12 description='Sophisticated open-source Object-Relational DBMS supporting almost all SQL constructs, including subselects, transactions, and user-defined types.' logo_url='http://hadoop.cesga.es/img/postgresql-icon.gif' x-auth-token:$TOKEN
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/12/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/12/options --data-binary @options.json -H "$AUTH"
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgresql/12/orchestrator --data-binary @fabfile.py -H "$AUTH"
 
 # Launching
-# http POST http://paas:6000/bigdata/api/v1/products/postgresql/9.2.15 password="verysecret" x-auth-token:$TOKEN
+# http POST http://paas:6000/bigdata/api/v1/products/postgresql/12 password="verysecret" x-auth-token:$TOKEN
 
